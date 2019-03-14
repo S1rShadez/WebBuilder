427d8d694f31177f53cd458b2026835302513441
S1rShadez <arthure97@hotmail.com>
2019-03-14 12:35:22 +0100

    Endret pop'ups
    Endret fra 3 separate pop'ups til en enkelt pop'up med alle tre feltene integrert. La også til noen kommentarer

--- Main.java
+++ Main.java
@@ -1,16 +1,54 @@
 package com.company;
 
+import javax.swing.*;
+
+import java.awt.*;
+
 import static javax.swing.JOptionPane.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        showMessageDialog(null, "Velkommen til WebBuilder!");
+        //Her lages innputfeltene
+        JTextField title = new JTextField(10);
+        JTextField header = new JTextField(10);
 
-        String title = showInputDialog("Skriv inn tittelen til Nettstedet:");
-        String header = showInputDialog("Skriv inn nettsidens Header:");
-        //Konverter de tre promptene over til èn dialogboks med flere input
-        //Legg også til andre valgmuligheter, for eks. ferdiggenerert nav bar
+        //Her lages "Panelet" som har all tekst on alle innputfeltene
+        JPanel mainPrompt = new JPanel();
+
+        //Her spesifiseres størrelsen og layout på "Panelet"
+        mainPrompt.setLayout(new GridLayout(0, 2, 2, 2));
+
+        //Her legges all tekst og innput til
+        mainPrompt.add(new Label("Velkommen til WebBuilder!"));
+        mainPrompt.add(Box.createVerticalStrut(10));
+        mainPrompt.add(new Label("Skriv inn tittelen til Nettstedet:"));
+        mainPrompt.add(title);
+        mainPrompt.add(new Label("Skriv inn nettsidens Header:"));
+        mainPrompt.add(header);
+
+        //Her legges en ok/cancel option til i "panelet"
+        int done = showConfirmDialog(null, mainPrompt, "", OK_CANCEL_OPTION);
+
+        //Her sjekkes det om cancel knappen ble trykket, hvis "ja" så avsluttes programmet
+        if(done == CANCEL_OPTION){
+            System.exit(8008135);
+        }
+
+        //Her hentes teksten som har blitt skrevet inn
+        String titleText = title.getText();
+        String headerText = header.getText();
+        
+        //Her sjekkes det for tomme tekstinnput, hvis de er tomme får brukeren en feilmelding, og programmet avsluttes
+        if(titleText == "" || headerText == "" || titleText.isEmpty() || headerText.isEmpty()){
+            showMessageDialog(null, "Vennligst fyll inn begge tekstfeltene!");
+            System.exit(3537);
+        }
+
+        /*
+        TODO
+        Legg også til andre valgmuligheter, for eks. ferdiggenerert nav bar
+        */
 
         int DBer = 0;
         try {
@@ -20,7 +58,7 @@ public class Main {
             System.exit(-1337);
         }
 
-        CreateFile.createFile(title,header,DBer);
+        CreateFile.createFile(titleText,headerText,DBer);
 
 	    showMessageDialog(null, "OBS!!!\nFilene har blitt lagret i den valgte mappen.\nKlikk \"OK\" for å lukke dette programmet");
     }
